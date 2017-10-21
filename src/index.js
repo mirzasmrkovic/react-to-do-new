ec99319db18441f8ec9850dd95830da7e0b3378b
mirzasmrkovic <mirza.smrkovic@gmail.com>
2017-10-21 14:48:37 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './animate.js'
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+registerServiceWorker();
+
+ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'))
