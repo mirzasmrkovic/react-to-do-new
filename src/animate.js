380f50088ce9b4b1aa4697bc2334a68ebd3ac0cc
mirzasmrkovic <mirza.smrkovic@gmail.com>
2017-11-29 18:09:02 +0100

    added animation swiping animation/transfered all html to js(loaded via reactdom)/added a new svg

--- src/animate.js
+++ src/animate.js
@@ -19,6 +19,35 @@ $(document).on('click', '.deleteItem', function(e) {
     marginTop: -10,
     elasticity: 0,
     duration: 1000,
+    display: 'none',
   })
   setTimeout(function(){$(item).remove()}, 450)
+})
+
+$(document).on('click', '.slideArrows', function(e) {
+  function slide(direction) {
+    let item = document.getElementById('selectedToDo').firstChild
+    anime({
+      targets: item,
+      translateX: function(el) {
+        console.log(el)
+        if (direction === 'left') {
+          return -200
+        }
+        else if (direction === 'right') {
+          return 200
+        }
+      },
+      opacity: 0,
+      elasticity: 0,
+      duration: 1000,
+    })
+    //setTimeout(function(){$(item).remove()}, 450)
+  }
+  if (this.id === 'leftSlideArrow') {
+    slide('left')
+  }
+  else if (this.id === 'rightSlideArrow') {
+    slide('right')
+  }
 })
